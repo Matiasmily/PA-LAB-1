fa971e4a1bbf860c6fc4a655628eca362e28b55b
Leonor-Rosales <158802869+Leonor-Rosales@users.noreply.github.com>
2024-07-17 21:24:33 -0600

    Se agregó el switch de suma y resta

--- Laboratorio 1 PA/Program.cs
+++ Laboratorio 1 PA/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numberOne = 0, numberTwo = 0;
+bool menuOptionTwo = true;
+do
+{
+    try
+    {
+        Console.Clear();
+        Console.WriteLine("--- Suma y Resta ---");
+        Console.WriteLine("1. Suma");
+        Console.WriteLine("2. Resta");
+        Console.WriteLine("3. Salir");
+        Console.Write("Ingrese una opción: ");
+        int optionCaseOne = int.Parse(Console.ReadLine());
+        switch (optionCaseOne)
+        {
+            case 1:
+                Console.Clear();
+                Console.Write("Ingrese un número: ");
+                numberOne = double.Parse(Console.ReadLine());
+                Console.Write("Ingrese otro número: ");
+                numberTwo = double.Parse(Console.ReadLine());
+                double additionOption = numberOne + numberTwo;
+                Console.WriteLine("El resultado de la suma es: " + additionOption);
+                Console.WriteLine("\nPresione enter para regresar");
+                Console.ReadKey();
+                menuOptionTwo = false;
+                break;
+            case 2:
+                Console.Clear();
+                Console.Write("Ingrese un número: ");
+                numberOne = double.Parse(Console.ReadLine());
+                Console.Write("Ingrese otro número: ");
+                numberTwo = double.Parse(Console.ReadLine());
+                double substractionOption = numberOne - numberTwo;
+                Console.WriteLine("El resultado de la suma es: " + substractionOption);
+                Console.WriteLine("\nPresione enter para regresar");
+                Console.ReadKey();
+                menuOptionTwo = false;
+                break;
+            case 3:
+                Console.Clear();
+                Console.WriteLine("Usted está saliendo del menú\nPresione Enter para continuar");
+                Console.ReadKey();
+                menuOptionTwo = false;
+                break;
+        }
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Ingrese una opción numérica\nPresione Enter para continuar");
+        Console.ReadKey();
+    } 
+} while (menuOptionTwo);
